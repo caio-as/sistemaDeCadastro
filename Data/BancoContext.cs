1c511f2581fb939cc2181c3bb35f71221ef6b473
caio-as <caio_as@yahoo.com.br>
2024-05-07 10:37:50 -0300

    subindo pasta local

---
+++ Data/BancoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using sistemaDeCadastro.Models;
+
+namespace sistemaDeCadastro.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ContatoModel> Contatos { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sistemaDeCadastro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
